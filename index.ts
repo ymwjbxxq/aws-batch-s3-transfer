019e0ad2f233795650ad1abc64f6a1421de5e9b9
Dan <dfrasca80@gmail.com>
2020-05-15 12:32:46 +0000

    fix reference

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
-import { CLIExecutionModule } from "./cliExecution";
+import { CLIExecution } from "./cliExecution";
 
 export const startExecution = async (): Promise<any> => {
-  const cli = new CLIExecutionModule();
+  const cli = new CLIExecution();
   const command = "aws s3 cp s3://myBucket/myFolder/ s3://myOtherBucket/myOtherFolder/ --recursive --region eu-central-1";
   await cli.execute(command);
 };
